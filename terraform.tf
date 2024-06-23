b2af88017ee6b94a9c7c27706d292cbb86bc288f
Siddharth <siddjos@amazon.com>
2024-06-24 00:15:03 +0530

    corrected terraform fmt

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,12 @@ terraform {
   #   }
   # }
 
-   backend "s3" {
-    bucket         = "my-demo-infra"
-    key            = "demo/state.tfstate"
-    region         = "us-west-2"
+  backend "s3" {
+    bucket = "my-demo-infra"
+    key    = "demo/state.tfstate"
+    region = "us-west-2"
     #dynamodb_table = "my-terraform-lock-table"
-    encrypt        = true
+    encrypt = true
   }
 
   required_providers {
--- variables.tf
+++ variables.tf
@@ -2,17 +2,17 @@ variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-west-2"
-  
+
 }
 
 variable "s3_bucket" {
   description = "S3 bucket name to store the state file"
-  type = string  
+  type        = string
 }
 
 variable "state_file_path" {
   description = "path of the state file"
-  type = string
-  
+  type        = string
+
 }
 
